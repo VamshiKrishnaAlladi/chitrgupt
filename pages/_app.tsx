5b17e2fe00a4ce908974305264c246a8b3bf6e55
Vamshi Krishna Alladi <vamshikrishnaalladi.me@gmail.com>
2021-04-03 22:55:05 +0530

    style(tailwind): initialize tailwind in the project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,9 @@ import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <Component {...pageProps} />
+        <div className="mx-auto w-5/6 sm:w-2/3 md:w-3/5">
+            <Component {...pageProps} />
+        </div>
     );
 }
 
