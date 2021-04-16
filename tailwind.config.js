cef39702360030dbd3f68a0ebd0d1a4a67404f31
Vamshi Krishna Alladi <vamshikrishnaalladi.me@gmail.com>
2021-04-16 18:01:22 +0530

    chore(package.json): install @tailwindcss/forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,9 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
 module.exports = {
     purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
     darkMode: false, // or 'media' or 'class'
-    theme: {
-        extend: {
-            fontFamily: {
-                rubik: ['Rubik', 'sans-serif'],
-            },
-        },
-    },
-    variants: {
-        extend: {
-        },
-    },
-    plugins: [],
+    theme: { extend: { fontFamily: { rubik: ['Rubik', 'sans-serif'] } } },
+    variants: { extend: {} },
+    plugins: [require('@tailwindcss/forms')({ strategy: 'class' })],
 };
